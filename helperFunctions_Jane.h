8dcee0c2e218e144928a633a796c6851c060a7d8
yshtalen <yshtalen@ucsc.edu>
2016-11-09 18:15:49 -0800

    Create helperFunctions_Jane.h

---
+++ helperFunctions_Jane.h
@@ -0,0 +1,23 @@
+#include "common.h"
+#include "Var.h"
+extern map<string, VAR*> createdVariables;
+
+void varHelper( stringstream &ss );
+void addHelper( stringstream &ss );
+void subHelper( stringstream &ss );
+void multHelper( stringstream &ss );
+void divHelper( stringstream &ss );
+void assignHelper( stringstream &ss );
+void outHelper( stringstream &ss );
+void setStrHelper( stringstream &ss );
+void getStrHelper( stringstream &ss );
+void labelHelper( stringstream &ss );
+void jmpHelper( stringstream &ss );
+void jmpZHelper( stringstream &ss );
+void jmpNZHelper( stringstream &ss );
+void jmpGTHelper( stringstream &ss );
+void jmpGTEHelper( stringstream &ss );
+void jmpLTHelper( stringstream &ss );
+void jmpLTEHelper( stringstream &ss );
+//void sleepHelper( stringstream &ss );
+void deleteVariables();
